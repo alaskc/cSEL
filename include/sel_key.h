8de305b03aed7e2b537eabe8af6113abeeb5bcde
alaskc <auritra2022@gmail.com>
2023-03-01 20:13:14 +0600

    Update intial

--- include/sel_key.h
+++ include/sel_key.h
@@ -1,23 +1,28 @@
 // Contains key data.
 
 #include "c_header.h"
+#include <string.h>
 
 #define __KEY_PRIVATE_ sizeof(char*) + 24 / __componet_sel_header
-#define __KEY_PUBLIC__ sizeof(char*) + 27 / __componet_sel_header
+#define __KEY_PUBLIC__ sizeof(char*) + 90 / __componet_sel_header
 
 typedef struct __header_key_0 {
-    char* key;
+    int* i_key;
+    int i_key_sum;
 } PublicKey;
 
 typedef struct __header_key_1 {
-    char* key;
+    int* i_key;
+    int i_key_sum;
 } PrivateKey;
 
 void generateLocker(PrivateKey *p0,char *password) {
     int sum = 0;
-    char* data;
+    int size = strlen(password);
+
+    for (int i = 0;i < size;i++) {}
 }
 
-void generateUnlocker(PrivateKey *p0 , PublicKey *p1) {
+void generatePublic(PrivateKey *p0 , PublicKey *p1) {
 
 }
